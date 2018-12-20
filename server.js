8bd8a8eb9bef38e227c509a3c6158b675244d8c4
RalphJbeily <ralph_jbeily@hotmail.com>
2018-12-20 14:59:43 +0200

    fix: add cors import

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./graphql');
